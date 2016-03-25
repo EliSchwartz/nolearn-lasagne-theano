5c4f83e887832af02849e11acd302b805f433fa5
Eli Schwartz <eli.shw@gmail.com>
2016-03-25 17:43:35 +0300

    added env creation

--- startup.sh
+++ startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+source env create
+source env update
 source activate nolearn-lasagne-theano
 
-#export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/cuda-7.5/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/cuda-7.5/lib64:$LD_LIBRARY_PATH
 export THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32,cuda.root=/usr/local/cuda-7.5
